f4e4207239afc275f61a70e09b287020c9afb434
Ivan Mirić <1009277+imiric@users.noreply.github.com>
2025-07-06 12:47:01 +0200

    feat(cli): add aliases to user subcommands

--- cli/user.go
+++ cli/user.go
@@ -14,31 +14,31 @@ import (
 type User struct {
 	Add struct {
 		Name string `arg:"" help:"The unique name of the user."`
-	} `kong:"cmd,help='Add a new user.'"`
-	Rm struct {
+	} `cmd:"" help:"Add a new user."`
+	Remove struct {
 		Name string `arg:"" help:"The unique name of the user."`
-	} `kong:"cmd,help='Remove a user.'"`
-	Ls struct{} `kong:"cmd,help='List users.'"`
+	} `cmd:"" aliases:"rm" help:"Remove a user."`
+	List struct{} `cmd:"" aliases:"ls" help:"List users."`
 }
 
 // Run the user command.
 func (c *User) Run(kctx *kong.Context, appCtx *actx.Context) error {
 	dbCtx := appCtx.DB.NewContext()
 
-	switch kctx.Args[1] {
-	case "add":
+	switch kctx.Command() {
+	case "user add <name>":
 		user := &models.User{Name: c.Add.Name}
 		if err := user.Save(dbCtx, appCtx.DB, false); err != nil {
 			return aerrors.NewRuntimeError(
 				fmt.Sprintf("failed adding user '%s'", c.Add.Name), err, "")
 		}
-	case "rm":
-		user := &models.User{Name: c.Rm.Name}
+	case "user remove <name>":
+		user := &models.User{Name: c.Remove.Name}
 		err := user.Delete(dbCtx, appCtx.DB)
 		if err != nil {
 			return err
 		}
-	case "ls":
+	case "user list":
 		users, err := models.Users(dbCtx, appCtx.DB, nil)
 		if err != nil {
 			return aerrors.NewRuntimeError("failed listing users", err, "")
