e13ccff244d2817fc9d6d9712cb82f57d6a6588f
Ivan Mirić <1009277+imiric@users.noreply.github.com>
2025-07-15 11:39:33 +0200

    feat(cli): add remote capabilities to the open command
    Remote firewall management is coming together nicely :)
    
    This is a milestone commit of the recent changes that brings it all
    together.
    
    It's now possible to securely add remote Sesame nodes and grant access
    to IP addresses using the API, via the CLI that leverages it.

--- cli/open.go
+++ cli/open.go
@@ -1,11 +1,14 @@
 package cli
 
 import (
+	"context"
 	"time"
 
 	actx "go.hackfix.me/sesame/app/context"
 	aerrors "go.hackfix.me/sesame/app/errors"
+	"go.hackfix.me/sesame/db/models"
 	"go.hackfix.me/sesame/firewall"
+	"go.hackfix.me/sesame/web/client"
 )
 
 // Open grants clients access to services.
@@ -14,30 +17,54 @@ type Open struct {
 	//nolint:lll // Long struct tags are unavoidable.
 	Clients  []string      `arg:"" required:"" help:"One or more client IP addresses in plain, CIDR or range notation. \n Examples: 10.0.0.10, 192.168.1.0/24, 172.16.1.10-172.16.1.100, a3:bc00::/32"`
 	Duration time.Duration `short:"d" help:"Duration of the access."`
+	Remote   string        `help:"Name of the remote Sesame node on which to grant access."`
 }
 
 // Run the open command.
 func (c *Open) Run(appCtx *actx.Context) error {
-	if !appCtx.Config.Firewall.Type.Valid {
-		return aerrors.NewWith(
-			"no firewall was configured on this system", "hint", "Did you forget to run 'sesame init'?")
-	}
-
 	ipSet, err := firewall.ParseToIPSet(c.Clients...)
 	if err != nil {
 		return err
 	}
 
-	_, fwMgr, err := firewall.Setup(
-		appCtx, appCtx.Config.Firewall.Type.V, appCtx.Config.Firewall.DefaultAccessDuration.V,
-	)
-	if err != nil {
-		return err
-	}
+	if c.Remote != "" {
+		r := &models.Remote{Name: c.Remote}
+		if err = r.Load(appCtx.DB.NewContext(), appCtx.DB); err != nil {
+			return err
+		}
 
-	err = fwMgr.AllowAccess(ipSet, c.ServiceName, c.Duration)
-	if err != nil {
-		return err
+		tlsConfig, err := r.ClientTLSConfig()
+		if err != nil {
+			return err
+		}
+
+		clientCtx, cancelClientCtx := context.WithTimeout(appCtx.Ctx, 10*time.Second)
+		defer cancelClientCtx()
+
+		client := client.New(r.Address, tlsConfig, appCtx.Logger)
+		err = client.Open(clientCtx, c.Clients, c.ServiceName, c.Duration)
+		if err != nil {
+			return err
+		}
+	} else {
+		if !appCtx.Config.Firewall.Type.Valid {
+			return aerrors.NewWith(
+				"no firewall was configured on this system", "hint", "Did you forget to run 'sesame init'?")
+		}
+
+		_, fwMgr, err := firewall.Setup(
+			appCtx, appCtx.Config.Firewall.Type.V, appCtx.Config.Firewall.DefaultAccessDuration.V, appCtx.Logger,
+		)
+		if err != nil {
+			return aerrors.NewWithCause(
+				"failed setting up firewall", err, "firewall_type", appCtx.Config.Firewall.Type.V)
+		}
+
+		err = fwMgr.AllowAccess(ipSet, c.ServiceName, c.Duration)
+		if err != nil {
+			return aerrors.NewWithCause(
+				"failed granting access", err, "firewall_type", appCtx.Config.Firewall.Type.V)
+		}
 	}
 
 	return nil
